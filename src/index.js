fe9742d14444ee52e060279321e6629c667ad235
ojegit <ojegit@gmail.com>
2021-09-17 14:27:13 +0300

    Added tags

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var addCommentButton = document.getElementById("add-comment");
+var removeCommentsButton = document.getElementById("remove-comments");
+var list = document.getElementById("list");
+
+// Add comment from the textarea to the list
+addCommentButton.addEventListener("click", function () {
+  var textAreaValue = document.getElementById("comment-area").value;
+  list.innerHTML += "<li>" + textAreaValue + "</li>";
+});
+
+// Remove all comments from the list
+removeCommentsButton.addEventListener("click", function () {
+  window.confirm("Are you sure you want to remove all comments?");
+  list.innerHTML = "";
+});
