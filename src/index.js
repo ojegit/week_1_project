25b9e53212588e217d9c1bd48bbfd273114b6b73
ojegit <ojegit@gmail.com>
2021-09-17 14:43:45 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import "./styles.css";
 
-var addCommentButton = document.getElementById("add-comment");
-var removeCommentsButton = document.getElementById("remove-comments");
-var list = document.getElementById("list");
+if (document.readyState !== "loading") {
+  // Document ready, executing
+  console.log("Document ready, executing");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    // Document was not ready, executing when loaded
+    console.log("Document ready, executing after a wait");
+    initializeCode();
+  });
+}
 
-// Add comment from the textarea to the list
-addCommentButton.addEventListener("click", function () {
-  var textAreaValue = document.getElementById("comment-area").value;
-  list.innerHTML += "<li>" + textAreaValue + "</li>";
-});
+function initializeCode() {
+  var addCommentButton = document.getElementById("add-comment");
+  var removeCommentsButton = document.getElementById("remove-comments");
+  var list = document.getElementById("list");
 
-// Remove all comments from the list
-removeCommentsButton.addEventListener("click", function () {
-  window.confirm("Are you sure you want to remove all comments?");
-  list.innerHTML = "";
-});
+  // Add comment from the textarea to the list
+  addCommentButton.addEventListener("click", function () {
+    var textAreaValue = document.getElementById("comment-area").value;
+    list.innerHTML += "<li>" + textAreaValue + "</li>";
+  });
+
+  // Remove all comments from the list
+  removeCommentsButton.addEventListener("click", function () {
+    window.confirm("Are you sure you want to remove all comments?");
+    list.innerHTML = "";
+  });
+}
